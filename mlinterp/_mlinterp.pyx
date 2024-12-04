a141f156fc3459fba9098d12ac545749e169b7a2
Nick Wogan <nicholaswogan@gmail.com>
2024-12-04 09:31:27 -0800

    no cpm

--- mlinterp/_mlinterp.pyx
+++ mlinterp/_mlinterp.pyx
@@ -1,7 +1,14 @@
 from numpy cimport ndarray
+import numpy as np
+cimport numpy as np
 from libc.stdlib cimport malloc, free
 
-cdef extern void interp_cpp(
+cdef extern void interp_vector(
+  int n, int *nd, double **xd, double *fd,
+  int ni, double *xi, double *fi
+)
+
+cdef extern void interp_single(
   int n, int *nd, double **xd, double *fd,
   double *xi, double *fi
 )
@@ -64,6 +71,7 @@ cdef class RegularGridInterpolator:
       if not sort:
         raise ValueError('Some of the arrays in `points` are not sorted')
 
+    assert values.dtype == np.double, "`values` must have have dtype `np.double`"
     cdef double *fd = <double *> values.data
     cdef int n1 = values.size
     self.fd = <double *> malloc(n1 * sizeof(double))
@@ -80,7 +88,46 @@ cdef class RegularGridInterpolator:
     if self.fd: 
       free(self.fd)
 
-  def __call__(self, ndarray[double, ndim=1] xi):
+  def evaluate_vector(self, ndarray[double, ndim=2] xi):
+
+    cdef int ni = xi.shape[0]
+    assert xi.shape[1] == self.n, "Input `xi` has the wrong dimension"
+    cdef ndarray[double,ndim=1] fi = np.empty(ni,np.double)
+
+    cdef double *xi_p
+    cdef ndarray[double,ndim=2] xi_copy
+    if np.PyArray_IS_C_CONTIGUOUS(xi):
+      xi_p = <double *> xi.data
+    else:
+      xi_copy = np.ascontiguousarray(xi)
+      xi_p = <double *> xi_copy.data
+
+    interp_vector(
+      self.n, self.nd, self.xd, self.fd, 
+      ni, xi_p, <double *> fi.data
+    )
+    return fi
+
+  def evaluate(self, ndarray[double, ndim=1] xi):
+
+    assert xi.shape[0] == self.n, "Input `xi` has the wrong dimension"
+    cdef double fi
+
+    cdef double *xi_p
+    cdef ndarray[double,ndim=1] xi_copy
+    if np.PyArray_IS_C_CONTIGUOUS(xi):
+      xi_p = <double *> xi.data
+    else:
+      xi_copy = np.ascontiguousarray(xi)
+      xi_p = <double *> xi_copy.data
+
+    interp_single(
+      self.n, self.nd, self.xd, self.fd, 
+      xi_p, &fi
+    )
+    return fi
+
+  def __call__(self, ndarray xi):
     """
     Interpolation at coordinates.
 
@@ -92,17 +139,18 @@ cdef class RegularGridInterpolator:
     Returns
     -------
     fi : float
-        Interpolated values at `x`.
+        Interpolated values at `xi`.
     """
 
-    cdef int ni = 1;
-    cdef double fi;
+    cdef double tmp;
+    cdef ndarray[double,ndim=1] fi
 
-    assert xi.shape[0] == self.n, "Input `x` has the wrong dimension"
-
-    interp_cpp(
-      self.n, self.nd, self.xd, self.fd, 
-      <double *> xi.data, &fi
-    )
+    if xi.ndim == 1:
+      tmp = self.evaluate(xi)
+      fi = np.array([tmp])
+    elif xi.ndim == 2:
+      fi = self.evaluate_vector(xi)
+    else:
+      raise ValueError("`xi` must have 1 or two dimensions.")
 
     return fi
--- mlinterp/mlinterp_c_api.cpp
+++ mlinterp/mlinterp_c_api.cpp
@@ -5,14 +5,14 @@ using namespace mlinterp;
 extern "C"
 {
 
-void interp_cpp(
+void interp_single(
   int n, int *nd, double **xd, double *fd,
   double *xi, double *fi
 )
 {
 
   // First, check for out of bounds
-  for (int i = 0; i < n; i ++)
+  for (int i = 0; i < n; i++)
   {
     if (xi[i] < xd[i][0] or xi[i] > xd[i][nd[i]-1])
     {
@@ -36,65 +36,122 @@ void interp_cpp(
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1]
+        xd[0], &xi[0], 
+        xd[1], &xi[1]
       );
       break;
     case 3:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2]
       );
       break;
     case 4:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3]
       );
       break;
     case 5:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4]
       );
       break;
     case 6:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4], xd[5], &xi[5]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4], 
+        xd[5], &xi[5]
       );
       break;
     case 7:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4], xd[5], &xi[5], xd[6], &xi[6]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4], 
+        xd[5], &xi[5], 
+        xd[6], &xi[6]
       );
       break;
     case 8:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4], xd[5], &xi[5], xd[6], &xi[6], xd[7], &xi[7]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4], 
+        xd[5], &xi[5], 
+        xd[6], &xi[6], 
+        xd[7], &xi[7]
       );
       break;
     case 9:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4], xd[5], &xi[5], xd[6], &xi[6], xd[7], &xi[7], xd[8], &xi[8]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4], 
+        xd[5], &xi[5], 
+        xd[6], &xi[6], 
+        xd[7], &xi[7], 
+        xd[8], &xi[8]
       );
       break;
     case 10:
       interp(
         nd, ni,
         fd, fi,
-        xd[0], &xi[0], xd[1], &xi[1], xd[2], &xi[2], xd[3], &xi[3], xd[4], &xi[4], xd[5], &xi[5], xd[6], &xi[6], xd[7], &xi[7], xd[8], &xi[8], xd[9], &xi[9]
+        xd[0], &xi[0], 
+        xd[1], &xi[1], 
+        xd[2], &xi[2], 
+        xd[3], &xi[3], 
+        xd[4], &xi[4], 
+        xd[5], &xi[5], 
+        xd[6], &xi[6], 
+        xd[7], &xi[7], 
+        xd[8], &xi[8], 
+        xd[9], &xi[9]
       );
       break;
+  }
+}
+
+void interp_vector(
+  int n, int *nd, double **xd, double *fd,
+  int ni, double *xi, double *fi
+)
+{
+
+  for (int i = 0; i < ni; i++)
+  {
+    interp_single(n, nd, xd, fd, &xi[i*n], &fi[i]);
   }
 
 }
